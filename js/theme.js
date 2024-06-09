56316d8a79312fc1725618a9b0e9c32774724f5c
Md Murad Khan <dev.muradkhan@gmail.com>
2024-06-09 16:06:32 +0600

    Fixed Toggle arrow issue with feu minor js format and neww Add CSS issues

--- js/theme.js
+++ js/theme.js
@@ -1,19 +1,25 @@
-jQuery(document).ready(function($){
-    $('#api-nav-dropdown').on("click",function(event){
-        $(this).find('span i').toggleClass('fa-solid fa-angle-up fa-solid fa-angle-down');
-        event.stopPropagation();
+(function( $ ) {
+	'use strict';
+	
+	jQuery(document).ready(function($){
+		$('#api-nav-dropdown').on("click",function(event){
+			$(this).find('span i').toggleClass('fa-solid fa-angle-up fa-solid fa-angle-down');
+			event.stopPropagation();
 
-        $(".dropdown-api-nav").toggle('fast');
+			$(".dropdown-api-nav").slideToggle();
 
-    });
+		});
 
-    $(".dropdown-api-nav").on("click", function (event) {
-        event.stopPropagation();
-    });
+		$(".dropdown-api-nav").on("click", function (event) {
+			event.stopPropagation();
+		});
 
-});
+	});
 
-jQuery(document).on("click", function () {
-    jQuery(".dropdown-api-nav").hide();
-});
+	$(document).on("click", function () {
+		$(".dropdown-api-nav").hide();
+        $('#api-nav-dropdown span i').addClass('fa-angle-down');
+        $('#api-nav-dropdown span i').removeClass('fa-angle-up');
+	});
 
+})( jQuery );
